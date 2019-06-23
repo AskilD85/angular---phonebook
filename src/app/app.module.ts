ecd29aafb7a08fee47f9159f9b61ea58b96e681f
askild85 <askildar@yandex.ru>
2019-06-23 14:32:29 +0500

    тел справчоник Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+ 
+import { HttpClientModule }   from '@angular/common/http';
+import { SearchPipe } from './search.pipe';
+//import { BrowserAnimaionsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:      [ BrowserModule, FormsModule, HttpClientModule, ],
+    declarations: [ AppComponent,SearchPipe],
+    bootstrap:    [ AppComponent ],
+    
 })
 export class AppModule { }
+
+
---
+++ src/app/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+    constructor(
+        public id?: number,
+        public Name?: string,
+        public phone_id?: number,
+        public room_id?: number) { }
+}
